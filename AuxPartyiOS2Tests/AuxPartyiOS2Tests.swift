b60213ee528806e2756ac1ac86440a6c54056041
taiseiak <taiseiklasen@gmail.com>
2017-01-12 13:47:31 -0800

    Initial Commit

---
+++ AuxPartyiOS2Tests/AuxPartyiOS2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  AuxPartyiOS2Tests.swift
+//  AuxPartyiOS2Tests
+//
+//  Created by Taisei Klasen on 1/12/17.
+//  Copyright © 2017 AuxParty. All rights reserved.
+//
+
+import XCTest
+@testable import AuxPartyiOS2
+
+class AuxPartyiOS2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
